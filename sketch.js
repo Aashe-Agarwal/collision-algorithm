d5d4a27d4b575993c1c1acda2635dad68f0682b8
Aashe-Agarwal <74452200+Aashe-Agarwal@users.noreply.github.com>
2020-11-21 18:11:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var movingRect, fixedRect;
+
+function setup() {
+  createCanvas(800,400);
+  movingRect=createSprite(700,200,70,70);
+  movingRect.shapeColor="blue";
+  fixedRect=createSprite(400, 200, 50, 50);
+  fixedRect.shapeColor="lightgreen";
+}
+
+function draw() {
+  background(0);
+  movingRect.x=mouseX;
+  movingRect.y=mouseY;
+  // console.log(movingRect.x - fixedRect.x ) ;
+  // console.log(movingRect.width/2+fixedRect.width/2);
+  if(movingRect.x - fixedRect.x < movingRect.width/2+fixedRect.width/2 && 
+    fixedRect.x -movingRect.x < movingRect.width/2+fixedRect.width/2 && movingRect.y - fixedRect.y
+     < movingRect.height/2+fixedRect.height/2 && 
+    fixedRect.y -movingRect.y < movingRect.height/2+fixedRect.height/2){
+   fixedRect.shapeColor="red";
+    movingRect.shapeColor="red";
+  }
+  else{
+    movingRect.shapeColor="blue";
+    fixedRect.shapeColor="lightgreen";
+  }
+  drawSprites();
+}
